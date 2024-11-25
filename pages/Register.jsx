6452ef074e45ad163fcab238ba32aa3a22b61e1c
John <johnlin8968@gmail.com>
2024-11-25 12:59:38 +1100

    Update Register.jsx

--- pages/Register.jsx
+++ pages/Register.jsx
@@ -6,9 +6,45 @@
 /**
  * Node modules
  */
+import { Link } from "react-router-dom";
+
+/**
+ * Components
+ */
+import PageTitle from "../src/components/PageTitle";
+
+/**
+ * Custom Modules
+ */
+import { logoLight, logoDark } from "../src/assets/assets.js";
 
 const Register = () => {
-  return <div></div>;
+  return (
+    <>
+      <PageTitle title="Create an account" />
+
+      <div className="">
+        <div className="">
+          <Link>
+            <img
+              src={logoLight}
+              alt="phoenix logo"
+              width={133}
+              height={24}
+              className=""
+            />
+            <img
+              src={logoLight}
+              alt="phoenix logo"
+              width={133}
+              height={24}
+              className=""
+            />
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Register;
