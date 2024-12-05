a0b6318208c1434522fb515de6aa494e2502c8b5
John <johnlin8968@gmail.com>
2024-12-05 22:52:54 +1100

    Create Snackbar.jsx

---
+++ src/components/Snackbar.jsx
@@ -0,0 +1,17 @@
+/**
+ * @copyright 2024 John Lin
+ * @license Apache-2.0
+ */
+
+/**
+ * Node modules
+ */
+import PropTypes from 'prop-types';
+
+const Snackbar = () => {
+  return (
+    <div className=''>
+      <span className=''></span>
+    </div>
+  );
+};
