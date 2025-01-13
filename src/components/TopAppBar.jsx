7515b89d740b524322e5a02b0ce19e69595b17f0
John <johnlin8968@gmail.com>
2025-01-13 16:12:13 +1100

    Created Avatar.jsx

--- src/components/TopAppBar.jsx
+++ src/components/TopAppBar.jsx
@@ -2,10 +2,21 @@
  * @copyright 2024 John Lin
  * @license Apache-2.0
  */
+
+/**
+ * Node modules
+ */
+import { Link } from 'react-router-dom';
+
 /**
  * Components
  */
 import { IconBtn } from './Button';
+
+/**
+ * Assets
+ */
+import { logoLight, logoDark } from '../assets/assets';
 
 const TopAppBar = () => {
   return (
@@ -15,6 +26,30 @@ const TopAppBar = () => {
           icon='menu'
           title='menu'
         />
+
+        <Link
+          to='/'
+          className=''
+        >
+          <img
+            src={logoLight}
+            width={133}
+            height={24}
+            alt='phoenix logo'
+            className='dark:hidden'
+          />
+          <img
+            src={logoDark}
+            width={133}
+            height={24}
+            alt='phoenix logo'
+            className='hidden dark:block'
+          />
+        </Link>
+      </div>
+
+      <div className='menu-wrapper'>
+        <IconBtn></IconBtn>
       </div>
     </header>
   );
