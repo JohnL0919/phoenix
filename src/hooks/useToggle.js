ce3016785d971fc40d8060817ad41ab9733a3e49
John <johnlin8968@gmail.com>
2025-01-22 15:11:04 +1100

    Update useToggle.js

--- src/hooks/useToggle.js
+++ src/hooks/useToggle.js
@@ -2,3 +2,24 @@
  * @copyright 2024 John Lin
  * @license Apache-2.0
  */
+
+/**
+ * Node Modules
+ */
+import { useState, useCallback } from 'react';
+
+/**
+ * A custom React hook for managing a toggle state.
+ * @returns {[boolean, Function]} An array containing the current toggle state (boolean) and a function to toggle the state.
+ */
+const useToggle = () => {
+  const [isOpen, setToggle] = useState(false);
+
+  const toggle = useCallback(() => {
+    setToggle((prev) => !prev);
+  }, []);
+
+  return [isOpen, toggle];
+};
+
+export { useToggle };
