7cc890a8aa4c5d58fbb1fbb7c619100587408b4b
John <johnlin8968@gmail.com>
2025-01-28 12:02:50 +1100

    Create logout.js

---
+++ src/routers/actions/utils/logout.js
@@ -0,0 +1,31 @@
+/**
+ * @copyright 2024 John Lin
+ * @license Apache-2.0
+ */
+
+/**
+ * Custom modules
+ */
+import { account } from '../lib/appwrite';
+
+/**
+ * Logs out the current user by deleting their session and navigates to the login page.
+ *
+ * @async
+ * @function logout
+ * @param {Function} navigate - The navigation function to redirect the user after logut.
+ * @returns {Promise<void>} - Returns a promise that resolves once the session is deleted and navigation occurs.
+ * @throws {Error} If there is an issue deleitng the user session, the error will be logged to the console.
+ */
+
+const logout = async (navigate) => {
+  try {
+    await account.deleteSession('current');
+  } catch (err) {
+    console.log(`Error deleting user session: ${err.message}`);
+  }
+
+  return navigate('/');
+};
+
+export default logout;
